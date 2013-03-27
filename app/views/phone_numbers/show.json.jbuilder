5b6b82ba81ed6a3da812caa6a06ed53aaf3d9736
Corey Kuwanoe <corey.kuwanoe@gmail.com>
2013-03-26 18:22:42 -0700

    first commit

---
+++ app/views/phone_numbers/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @phone_number, :created_at, :updated_at
---
+++ lib/do_not_call.rb
@@ -0,0 +1,24 @@
+require 'rubygems'
+require 'mechanize'
+require 'open-uri'
+
+module DoNotCallApi
+  module Client
+    def add_to_do_not_call_list(phone)
+      email = "dnkcityusa+#{rand(1...99999)}@gmail.com" #set email address using random number after the +
+      agent = Mechanize.new
+      agent.get("https://donotcall.gov/register/reg.aspx")
+
+      #find the first form and set the variables, then submit
+      form = agent.page.forms[0]
+      form["PhoneNumberTextBox1"] = phone
+      form["EmailAddressTextBox"] = email
+      form["ConfirmEmailAddressTextBox"] = email
+      form.click_button
+
+      # click the confirm button on confirmation screen
+      confirm = agent.page.forms[0]
+      confirm.click_button
+    end
+  end
+end
